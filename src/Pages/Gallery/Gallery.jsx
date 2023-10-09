6cf2cbc538c0a3086e1c6a9d0ba5b5924c5b6810
Md.Ashikur Rahman (Showrov) <showrov.cse.cou@gmail.com>
2023-10-09 07:33:09 +0600

    Added Gallery and responsive

--- src/Pages/Gallery/Gallery.jsx
+++ src/Pages/Gallery/Gallery.jsx
@@ -1,7 +1,44 @@
+import { useEffect, useState } from "react";
+import Aos from "aos";
+import "aos/dist/aos.css";
+
 const Gallery = () => {
+  const [gallery, setGallery] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    Aos.init();
+    fetch("/images.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setGallery(data);
+        setLoading(false);
+      });
+  }, []);
   return (
     <div>
-      <h1>This is Gallery</h1>
+      <h1 className=" text-center pt-8 pb-2 text-2xl md:text-4xl font-bold">
+        <span className="text-color-secondary">Cvents</span>{" "}
+        <span>Gallery</span>
+      </h1>
+      <p className="px-[5%] pb-6 text-center text-base md:text-lg">
+        Capturing the Summary of Our Corporate Gatherings
+      </p>
+
+      {loading ? (
+        <div className="h-[33vh] flex justify-center items-center">
+          <span className="loading loading-dots loading-lg"></span>
+        </div>
+      ) : (
+        <div className="px-[5%] pb-10 grid grid-cols-4 gap-1 md:gap-2">
+          {gallery.map((item) => (
+            <div key={item.id}>
+              <div className="overflow-hidden border-2 border-solid border-color-gray rounded-lg">
+                <img src={item.imageUrl} alt="" />
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
--- src/Pages/Services/Services.jsx
+++ src/Pages/Services/Services.jsx
@@ -18,27 +18,6 @@ const Services = () => {
         ))}
       </div>
     </div>
-    // <div
-    //   className="card card-compact bg-color-primary-dark border-2 border-solid border-color-gray"
-    //   data-aos="zoom-in"
-    // >
-    //   <figure className="h-60 w-full">
-    //     <img className="h-full w-full" src={service.image} alt="" />
-    //   </figure>
-    //   <div className="card-body">
-    //     <h2 className="card-title">{service.name}</h2>
-    //     <p>{service.description}</p>
-    //     <p>
-    //       <span className="font-bold">Duration:</span> {service.duration}
-    //     </p>
-    //     <p>
-    //       <span className="font-bold">Price:</span> {service.pricing}
-    //     </p>
-    //     <div className="card-actions justify-end">
-    //       <button className="signbtn">View Details</button>
-    //     </div>
-    //   </div>
-    // </div>
   );
 };
 
